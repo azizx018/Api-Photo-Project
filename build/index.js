24af4acef74b3c43764836f914f902ddf7110c2a
Chiara Aziz <chiara.aziz@gmail.com>
2021-08-15 12:56:37 -0500

    checks if the query values are valid

--- build/index.js
+++ build/index.js
@@ -6,8 +6,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
 var port = 3000;
+var sharp = require('sharp');
+var inputPhoto = __dirname + '/photos/full/fjord.jpeg';
+var outputPhoto = __dirname + '/photos/thumb/fjord.jpeg';
+// fs.exists(inputPhoto, (exist) => {
+//     if (exist) {
+//         console.log("I got your file")
+//     } else {
+//         console.log("no file")
+//     }
+// });
+// console.log(__dirname)
 app.get('/api', function (req, res) {
-    res.send('hello world');
+    res.send(
+        sharp(inputPhoto)
+        .resize(200, 200)
+        .toFile(outputPhoto, function (err, info) {
+        // output.jpg is a 200 pixels wide and 200 pixels high image
+        // containing a scaled and cropped version of input.jpge
+    }));
 });
 app.listen(port, function () {
     console.log("server is working on local host" + port);

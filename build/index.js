09ee5b860a86e33f75840d5b7b78b7d7a8f30909
Chiara Aziz <chiara.aziz@gmail.com>
2021-08-15 21:52:46 -0500

    program works!

--- build/index.js
+++ build/index.js
@@ -4,27 +4,77 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var fs_1 = __importDefault(require("fs"));
+;
 var app = express_1.default();
 var port = 3000;
 var sharp = require('sharp');
-var inputPhoto = __dirname + '/photos/full/fjord.jpeg';
-var outputPhoto = __dirname + '/photos/thumb/fjord.jpeg';
-// fs.exists(inputPhoto, (exist) => {
-//     if (exist) {
-//         console.log("I got your file")
-//     } else {
-//         console.log("no file")
-//     }
-// });
-// console.log(__dirname)
+var photoDir = __dirname + '/photos/';
+var fullDir = photoDir + 'full/';
+var thumbDir = photoDir + 'thumb/';
+app.use('/photos', express_1.default.static(__dirname + '/photos'));
 app.get('/api', function (req, res) {
-    res.send(
-        sharp(inputPhoto)
-        .resize(200, 200)
-        .toFile(outputPhoto, function (err, info) {
-        // output.jpg is a 200 pixels wide and 200 pixels high image
-        // containing a scaled and cropped version of input.jpge
-    }));
+    var errors = [];
+    //check for query string that all variables exist
+    var fileName = req.query.filename;
+    if (fileName === undefined || fileName === null || Object.keys(fileName).length === 0) {
+        console.log("filename needed in query string");
+        errors.push("filename needed in query string");
+    }
+    else {
+        console.log("the filename entered is " + fileName);
+    }
+    //check that a width was entered as a number
+    //Need to check if this value is numberic
+    var width = req.query.width;
+    if (width === undefined || width === null || Object.keys(width).length === 0
+        || Object.is(NaN, parseInt(width.toString()))) {
+        console.log("valid width needed in query string");
+        errors.push("valid width needed in query string");
+    }
+    else {
+        console.log("the width entered is " + width);
+    }
+    //this checks that the height has a value and is a valid number
+    var height = req.query.height;
+    if (height === undefined || height === null || Object.keys(height).length === 0
+        || Object.is(NaN, parseInt(height.toString()))) {
+        console.log("valid height needed in query string");
+        errors.push("valid height needed in query string");
+    }
+    else {
+        console.log("the width entered is " + height);
+    }
+    //check if the file exists-- assuming all files are jpeg
+    if (fs_1.default.existsSync(fullDir + fileName + '.jpeg')) {
+        console.log("The file exists");
+    }
+    else {
+        console.log("file does not exist");
+        errors.push("file does not exist");
+    }
+    //if there are any errors res.send them
+    if (errors.length > 0) {
+        res.send(errors);
+    }
+    else {
+        //see if the thumbnail doesn't exist then run sharp
+        var thumbFileName = "" + thumbDir + fileName + "_" + width + "x" + height + ".jpeg";
+        console.log(thumbFileName);
+        var thumbExists = fs_1.default.existsSync(thumbFileName);
+        if (!thumbExists) {
+            console.log("Hey I'm misssing!");
+            //run the image through sharp
+            sharp(fullDir + fileName + '.jpeg')
+                .resize(200, 200)
+                .toFile(thumbFileName, function (err, info) {
+                // output.jpg is a 200 pixels wide and 200 pixels high image
+                // containing a scaled and cropped version of input.jpge
+            });
+        }
+        //render photo
+        res.send();
+    }
 });
 app.listen(port, function () {
     console.log("server is working on local host" + port);
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
 import express from 'express';
 import fs from 'fs';;
-import url from 'url';
 
 const app = express();
 const port = 3000;
 
+
 const sharp = require('sharp');
-const inputPhoto = __dirname + '/photos/full/';
-const outputPhoto = __dirname + '/photos/thumb/';
+const photoDir = __dirname + '/photos/'
+const fullDir = photoDir + 'full/'
+const thumbDir = photoDir + 'thumb/'
 
 
-//app.use('/thumb', express.static(__dirname + '/photos'))
+
+app.use(express.static(thumbDir))
+//app.use('/photos', express.static('thumb'));
+
 app.get('/api', (req, res) => {
-
+    const errors = []
     //check for query string that all variables exist
     const fileName = req.query.filename
     if (fileName === undefined || fileName === null || Object.keys(fileName).length === 0) {
-        console.log("I need a filename")
+        console.log("filename needed in query string")
+        errors.push("filename needed in query string")
     } else {
         console.log(`the filename entered is ${fileName}`)
     }
@@ -25,45 +30,65 @@ app.get('/api', (req, res) => {
     const width = req.query.width
     if (width === undefined || width === null || Object.keys(width).length === 0
         || Object.is(NaN, parseInt(width.toString()))) {
-        console.log("I need a valid width")
+        console.log("valid width needed in query string")
+        errors.push("valid width needed in query string")
     } else {
         console.log(`the width entered is ${width}`)
     }
-    //this checks is the height value is a valid number
+    //this checks that the height has a value and is a valid number
     const height = req.query.height
     if (height === undefined || height === null || Object.keys(height).length === 0
         || Object.is(NaN, parseInt(height.toString()))) {
-        console.log("I need a valid height")
+        console.log("valid height needed in query string")
+        errors.push("valid height needed in query string")
     } else {
         console.log(`the width entered is ${height}`)
     }
 
+    //check if the file exists-- assuming all files are jpeg
+    if (fs.existsSync(fullDir + fileName + '.jpeg')) {
+        console.log("The file exists")
+    } else {
+        console.log("file does not exist")
+        errors.push("file does not exist")
+    }
 
-    //check if the file exists
-    // fs.exists(`inputPhoto + ${fileName}`, (exist) => {
-    //     if (exist) {
-    //         console.log("I got your file")
-    //     } else {
-    //         console.log("no file")
-    //     }
-    // });
-    // console.log(__dirname)
     //if there are any errors res.send them
-    //see if the thumbnail doesn't exist then run sharp 
-    //render photo
+    if (errors.length > 0) {
+        res.send(errors)
+    } else {
+        //see if the thumbnail doesn't exist then run sharp
+        const thumbFileName = `${fileName}_${width}x${height}.jpeg`
+        const thumbFilePath = `${thumbDir}${thumbFileName}`
+        const thumbExists = fs.existsSync(thumbFilePath)
+        if (!thumbExists) {
+            console.log("Hey I'm misssing!")
+            //run the image through sharp and render photo
+            sharp(fullDir + fileName + '.jpeg')
+                .resize(Number(width), Number(height))
+                .toFile(thumbFilePath, function (err: Error, info: Object) {
+                    if (err === null) {
+                        res.send(
+                            `<img src="${thumbFileName}" />`
+                        )
+                    } else {
+                        res.send(err)
+                    }
+                })
+        } else {
+            //image exists ---show image
+            res.send(
+                `<img src="${thumbFileName}" />`
+            )
 
-    sharp(inputPhoto)
-        .resize(200, 200)
-        .toFile(outputPhoto, function (err: Error, info: Object) {
-            // output.jpg is a 200 pixels wide and 200 pixels high image
-            // containing a scaled and cropped version of input.jpge
-        })
-    res.send();
-});
+        }
+
+    }
+})
 
 
 
 
 app.listen(port, () => {
     console.log(`server is working on local host${port}`);
-});
+})
